7ebc2da96fa151b63edee356aaabb52d9046cb48
Noe Gebert <noe-daniel.gebert@epitech.com>
2025-06-02 15:30:35 +0200

    fix(server): main

--- server/src/main.c
+++ server/src/main.c
@@ -7,29 +7,12 @@
 
 #include "../include/server.h"
 
-void test()
-{
-	client_list_t *list = init_client_list();
-
-	cl_display(list);
-
-	cl_add_end(list, 3);
-	cl_add_end(list, 2);
-	// cl_add_end(list, 5);
-	cl_display(list);
-
-	cl_remove(list, 2);
-	cl_add_end(list, 3);
-	cl_display(list);
-}
-
 int main(void)
 {
-	test();
-	// server_t *server = init_server();
+	server_t *server = init_server();
 
-	// if (server == NULL)
-	// 	return 84;
-	// server_run(server);
+	if (server == NULL)
+		return 84;
+	server_run(server);
 	return 0;
 }
